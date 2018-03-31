05e7f3e0daeece9266e0fc92b0c1008f7b8e77b5
BBaoVanC <BBaoVanC@gmail.com>
2018-03-31 09:46:13 -0500

    v1.2 Fixed code to better fit the PEP-8 line limit requirements so Atom will stop yelling at me.

--- namegen.py
+++ namegen.py
@@ -18,50 +18,58 @@ import random
 import urllib.request
 
 
-if not os.path.isfile("desiquintans.com_nounlist.txt"):  # if the noun list doesn't exist
-    urllib.request.urlretrieve("http://benstar.wc.lt/random_files/desiquintans.com_nounlist.txt",
-                               "desiquintans.com_nounlist.txt")  # download the noun list
+# this is the url of the noun list
+nouns_url = "http://benstar.wc.lt/random_files/desiquintans.com_nounlist.txt"
+# if the list doesn't exist
+if not os.path.isfile("desiquintans.com_nounlist.txt"):
+    # download the noun list
+    urllib.request.urlretrieve(nouns_url, "desiquintans.com_nounlist.txt")
 
 
 def generate(count, debug):
     with open("desiquintans.com_nounlist.txt") as f:  # open the word list file
         words = f.readlines()  # read all lines into a list
     words = [x.strip() for x in words]  # remove the "\n" from each word
-    wsuffixes = ["ator", "man", "guy", "ifier", "anator", ""]  # create a list of suffixes we can use
-    wprefixes = ["Mr", "The", ""]  # create a list of prefixes we can use
-    names = list()  # create blank list of generated names
+    wsuffixes = ["ator", "man", "guy", "ifier", "anator", ""]  # suffixes
+    wprefixes = ["Mr", "The", ""]  # prefixes
+    names = list()  # initialize the names list variable
 
     # vowels = ['a', 'e', 'i', 'o', 'u']
     # vowels2 = ['a', 'e', 'i', 'o', 'u', 'y']
     vowels3 = ['a', 'i', 'o', 'u', 'y']
     n = 1
     while count >= n:
-        word_pre = random.choice(words)  # choose a random word from the list of words
+        word_pre = random.choice(words)  # choose a random word
         word_pre = word_pre.capitalize()  # capitalize the first letter
-        wprefix = random.choice(wprefixes)  # choose a random prefix from the list
-        wsuffix = random.choice(wsuffixes)  # choose a random suffix from the list
+        wprefix = random.choice(wprefixes)  # choose a random prefix
+        wsuffix = random.choice(wsuffixes)  # choose a random suffix
         wsuffix2 = wsuffix
         if wsuffix == "ator" or wsuffix == "anator":
             if random.choice([True, False, False]):
                 wsuffix2 = wsuffix + "ifier"  # add the extra suffix of "ifier"
 
-        caps = random.choice([True, False, False, False])  # if we should make the name all caps
+        # if we should make the name all caps
+        caps = random.choice([True, False, False, False])
         if caps:  # if we are going to make it all caps
             wprefix = wprefix.upper()  # set the prefix to all caps
             word_pre = word_pre.upper()  # set the word to all caps
             wsuffix2 = wsuffix2.upper()  # set the suffix to all caps
             if len(wprefix) > 0:  # if there is a prefix
-                wprefix = wprefix + "_"  # add an underscore to the end of the prefix
+                wprefix = wprefix + "_"  # underscore at the end of the prefix
 
-        if word_pre[-1] in vowels3:  # if the word ends in a vowel (including 'y')
-            if wsuffix == 'ator' or wsuffix == 'anator':  # if the randomly chosen suffix is "ator" or "anator"
+        if word_pre[-1] in vowels3:  # if the word ends in a vowel (aeiouy)
+            if wsuffix == 'ator' or wsuffix == 'anator':  # grammar fix
                 wsuffix = 'nator'  # set the suffix to "nator"
 
-        word = str(wprefix) + str(word_pre) + str(wsuffix2)  # create a string 'word' which adds the 3 parts together
-        space = random.choice(["_", "", ""])  # choose if there should be a space before the number at the end
-        number = random.randint(0, 999)  # choose a random number to have at the end
-        name = str(word) + str(space) + str(number)  # puts together the different parts of the username
-        if debug:
+        # create a string 'word' which adds the 3 parts together
+        word = str(wprefix) + str(word_pre) + str(wsuffix2)
+        # choose if there should be a space before the number at the end
+        space = random.choice(["_", "", ""])
+        # choose a random number to have at the end
+        number = random.randint(0, 999)
+        # puts together the different parts of the username
+        name = str(word) + str(space) + str(number)
+        if debug:  # if debug mode is enabled
             print("Generated name: " + name)
         names.append(name)  # add name to list of generated names
         n = n + 1  # increases our loop counter
@@ -71,33 +79,36 @@ def generate(count, debug):
 
 if __name__ == '__main__':  # if the program wasn't run as an import
     print("Generating names")
-    usernames = generate(100, True)  # use our function to generate 100 names into the variable usernames
+    # use our function to generate 100 names into the variable usernames
+    usernames = generate(100, True)
     print("Opening file")
-    file = open("names.txt", "w+")  # open the list of names. + means to create the file if it doesn't exist
+    # open the list of names. + means to create the file if it doesn't exist
+    file = open("names.txt", "w+")
 
     usernames2 = list()  # create new list named usernames2
     for item in usernames:
-        item = item + "\n"  # add newline character to each item in the usernames2 list...
+        # add newline character to each item in the usernames2 list...
+        item = item + "\n"
         usernames2.append(item)  # ...
-    usernames2[-1] = usernames2[-1].strip()  # remove newline character from last item in list
+    usernames2[-1] = usernames2[-1].strip()  # remove newline from last item
     for item in usernames2:
         file.write(item)  # write each username to the file
         # print("Writing name: " + item)
 
-    # ================================UNOPTIMIZED ALGORITHM================================ #
-    # usernames2 = list()  # create new empty list named usernames2                         #
-    # for item in usernames:                                                                #
-    #     item = item + "\n"  # add a newline character to the end of each username         #
-    #     usernames2.append(item)                                                           #
-    # lastname = usernames2[-1]  # set variable lastname to the last name in usernames2     #
-    # lastname = lastname.strip()  # remove newline character from lastname                 #
-    # usernames2 = usernames2[:-1]  # remove last item from usernames2                      #
-    # usernames2.append(lastname)  # add lastname to usernames2                             #
-    #                                                                                       #
-    # for username in usernames2:  # for each username in the list of usernames             #
-    #     file.write(username)  # write username to file along with a newline character     #
-    # ================================UNOPTIMIZED ALGORITHM================================ #
+    # ================================UNOPTIMIZED ALGORITHM================== #
+    # usernames2 = list()  # create new empty list named usernames2           #
+    # for item in usernames:                                                  #
+    #     item = item + "\n"  # add a newline to the end of each name         #
+    #     usernames2.append(item)                                             #
+    # lastname = usernames2[-1]  # set variable lastname to the last name     #
+    # lastname = lastname.strip()  # remove newline character from lastname   #
+    # usernames2 = usernames2[:-1]  # remove last item from usernames2        #
+    # usernames2.append(lastname)  # add lastname to usernames2               #
+    #                                                                         #
+    # for username in usernames2:  # for each username                        #
+    #     file.write(username)  # write username to the file                  #
+    # ================================UNOPTIMIZED ALGORITHM================== #
 
     print("Saving file")
-    file.close()  # close the file so other programs can access it and apply our changes
+    file.close()  # close the file and apply our changes
     print("Finished!")
