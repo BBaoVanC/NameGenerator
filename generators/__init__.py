f8b8ebf855e7b4bf8ebeef2b2626dbcf75ccc231
BBaoVanC <BBaoVanC@gmail.com>
2019-05-28 14:50:05 -0500

    v2.4.5 - Created generators package You can now use generators as a package. To do this, your program must not be inside the generators package folder. Then, just 'import generators' and use generators.[method].gen(args).

---
+++ generators/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Name Generator Method Package
+
+by BBaoVanC
+
+!!! THIS PACKAGE WILL NOT WORK IF YOUR PROGRAM IS IN generators/ !!!
+This is a package which contains the various name generation methods used in
+https://github.com/BBaoVanC/NameGenerator
+"""
+
+# Imports
+import generators.classic
+import generators.random
+import generators.v2
