14228989e779fd9a87f96c678b2aa83a3a80aebf
BBaoVanC <BBaoVanC@gmail.com>
2019-11-29 16:27:32 -0600

    v2.5.3_2 - Removed Python version specification Windows didn't like it. Thanks Windows.

--- travis.sh
+++ travis.sh
@@ -1,3 +1,3 @@
-python3 namegen.py debug=True file=classic.txt method=classic
-python3 namegen.py debug=True file=v2.txt method=v2
-python3 namegen.py debug=True file=random.txt method=random
+python namegen.py debug=True file=classic.txt method=classic
+python namegen.py debug=True file=v2.txt method=v2
+python namegen.py debug=True file=random.txt method=random
