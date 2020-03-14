315f29e00a05acaf51fa74f79c7e9718e464ec97
BBaoVanC <BBaoVanC@gmail.com>
2020-03-13 20:05:26 -0500

    Created scripts to regenerate names This makes the periodic regenetation of example names easier.

---
+++ regenerate-names.bat
@@ -0,0 +1,2 @@
+py -3 namegen.py debug=True file=example-names/classic.txt method=classic
+py -3 namegen.py debug=True file=example-names/random.txt method=random
---
+++ regenerate-names.sh
@@ -0,0 +1,2 @@
+python3 namegen.py debug=True file=example-names/classic.txt method=classic
+python3 namegen.py debug=True file=example-names/random.txt method=random
