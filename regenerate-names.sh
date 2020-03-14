9494a46dfed913e546527196a8efbdb7eaf16e32
BBaoVanC <bbaovanc@protonmail.com>
2020-03-13 20:55:21 -0500

    Created scripts to regenerate example names (#12)
    * Created scripts to regenerate names
    This makes the periodic regeneration of example names easier.
    
    * Add bash shebang to regenerate-names.sh
    
    * Added regenerate-names.sh and echo to travis.sh
    Added testing of regenerate-names.sh and echo commands to clean up the logs.
    
    * Specify bash in travis.sh

---
+++ regenerate-names.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+python3 namegen.py debug=True file=example-names/classic.txt method=classic
+python3 namegen.py debug=True file=example-names/random.txt method=random
--- travis.sh
+++ travis.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+echo "TESTING CLASSIC GENERATOR:"
 python3 namegen.py debug=True file=classic.txt method=classic
+echo "TESTING RANDOM GENERATOR:"
 python3 namegen.py debug=True file=random.txt method=random
+echo "TESTING REGENERATE NAMES SCRIPT:"
+bash ./regenerate-names.sh
