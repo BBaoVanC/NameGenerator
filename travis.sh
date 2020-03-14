b473ba75de9dec6311afeb227bf6cc2ed211eb98
BBaoVanC <bbaovanc@protonmail.com>
2020-03-13 20:40:44 -0500

    Added regenerate-names.sh and echo to travis.sh Added testing of regenerate-names.sh and echo commands to clean up the logs.

--- travis.sh
+++ travis.sh
@@ -1,2 +1,6 @@
+echo "TESTING CLASSIC GENERATOR:\n"
 python3 namegen.py debug=True file=classic.txt method=classic
+echo "TESTING RANDOM GENERATOR:\n"
 python3 namegen.py debug=True file=random.txt method=random
+echo "TESTING REGENERATE NAMES SCRIPT:\n"
+./regenerate-names.sh
