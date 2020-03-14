122bb7d56344831a2ad3166e62907908d9f61e5f
BBaoVanC <bbaovanc@protonmail.com>
2020-03-13 20:44:15 -0500

    Specify bash in travis.sh

--- travis.sh
+++ travis.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 echo "TESTING CLASSIC GENERATOR:\n"
 python3 namegen.py debug=True file=classic.txt method=classic
 echo "TESTING RANDOM GENERATOR:\n"
 python3 namegen.py debug=True file=random.txt method=random
 echo "TESTING REGENERATE NAMES SCRIPT:\n"
-./regenerate-names.sh
+bash ./regenerate-names.sh
