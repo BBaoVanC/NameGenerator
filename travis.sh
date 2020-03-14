df534dffdfea4ad638b972b318a72f2269dd1a36
BBaoVanC <bbaovanc@protonmail.com>
2020-03-13 20:49:32 -0500

    Remove '\n' from travis.sh It didn't print right.

--- travis.sh
+++ travis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-printf "TESTING CLASSIC GENERATOR:\n"
+printf "TESTING CLASSIC GENERATOR:"
 python3 namegen.py debug=True file=classic.txt method=classic
-printf "TESTING RANDOM GENERATOR:\n"
+printf "TESTING RANDOM GENERATOR:"
 python3 namegen.py debug=True file=random.txt method=random
-printf "TESTING REGENERATE NAMES SCRIPT:\n"
+printf "TESTING REGENERATE NAMES SCRIPT:"
 bash ./regenerate-names.sh
