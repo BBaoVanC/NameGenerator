5974fc223cd1d8a65ec3e047aadfaf3fe0caf3ee
BBaoVanC <bbaovanc@protonmail.com>
2020-03-15 14:03:42 -0500

    Fix ShellCheck SC2181 Warning

--- regenerate-names.sh
+++ regenerate-names.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-python3 namegen.py debug=True file=example-names/classic.txt method=classic
-if [ ! $? == 0 ]; then
+if ! python3 namegen.py debug=True file=example-names/classic.txt method=classic; then
     echo "ERROR DETECTED"
     exit 1
 fi
-python3 namegen.py debug=True file=example-names/random.txt method=random
-if [ ! $? == 0 ]; then
+if ! python3 namegen.py debug=True file=example-names/random.txt method=random; then
     echo "ERROR DETECTED"
     exit 1
 fi
--- testing-run.sh
+++ testing-run.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 echo "TESTING CLASSIC GENERATOR:"
-python3 namegen.py debug=True file=classic.txt method=classic
-if [ ! $? == 0 ]; then
+if ! python3 namegen.py debug=True file=classic.txt method=classic; then
     echo "ERROR DETECTED"
     exit 1
 fi
 echo "TESTING RANDOM GENERATOR:"
-python3 namegen.py debug=True file=random.txt method=random
-if [ ! $? == 0 ]; then
+if ! python3 namegen.py debug=True file=random.txt method=random; then
     echo "ERROR DETECTED"
     exit 1
 fi
