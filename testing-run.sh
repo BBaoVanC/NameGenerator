7ae75ae7a1e1644823ba79145d9128810d0711ed
BBaoVanC <bbaovanc@protonmail.com>
2020-03-14 23:40:18 -0500

    Integrated CircleCI testing (#25)
    Test the NameGen CLI and regenerate-names.sh script on the latest three versions of Python (currently 3.6.9, 3.7.7, and 3.8.2).

--- testing-run.sh
+++ testing-run.sh
@@ -4,4 +4,3 @@ python3 namegen.py debug=True file=classic.txt method=classic
 echo "TESTING RANDOM GENERATOR:"
 python3 namegen.py debug=True file=random.txt method=random
 echo "TESTING REGENERATE NAMES SCRIPT:"
-bash ./regenerate-names.sh
