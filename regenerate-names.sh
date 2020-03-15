04575c70e7885261052e30b237c976b446f734b4
BBaoVanC <bbaovanc@protonmail.com>
2020-03-15 14:12:21 -0500

    Reenable error check on regenerate names script

--- regenerate-names.sh
+++ regenerate-names.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# if ! python3 namegen.py debug=True file=example-names/classic.txt method=classic; then
-#     echo "ERROR DETECTED"
-#     exit 1
-# fi
+if ! python3 namegen.py debug=True file=example-names/classic.txt method=classic; then
+    echo "ERROR DETECTED"
+    exit 1
+fi
 if ! python3 namegen.py debug=True file=example-names/random.txt method=random; then
     echo "ERROR DETECTED"
     exit 1
