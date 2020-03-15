7c82d1e7010ff96006b5858e878baa3e08f77775
BBaoVanC <bbaovanc@protonmail.com>
2020-03-15 14:06:37 -0500

    [Test] enable check in testing-run.sh

--- testing-run.sh
+++ testing-run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "TESTING CLASSIC GENERATOR:"
-# if ! python3 namegen.py debug=True file=classic.txt method=classic; then
-#     echo "ERROR DETECTED"
-#     exit 1
-# fi
+if ! python3 namegen.py debug=True file=classic.txt method=classic; then
+    echo "ERROR DETECTED"
+    exit 1
+fi
 echo "TESTING RANDOM GENERATOR:"
 if ! python3 namegen.py debug=True file=random.txt method=random; then
     echo "ERROR DETECTED"
