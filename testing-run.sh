97321e13ce32d1c5697ade2625b4add1453f18fd
BBaoVanC <bbaovanc@protonmail.com>
2020-03-15 13:55:23 -0500

    Exit 1 if testing scripts have an error

--- testing-run.sh
+++ testing-run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 echo "TESTING CLASSIC GENERATOR:"
 python3 namegen.py debug=True file=classic.txt method=classic
+if [ ! $? == 0 ]; then
+    echo "ERROR DETECTED"
+    exit 1
+fi
 echo "TESTING RANDOM GENERATOR:"
 python3 namegen.py debug=True file=random.txt method=random
-echo "TESTING REGENERATE NAMES SCRIPT:"
+if [ ! $? == 0 ]; then
+    echo "ERROR DETECTED"
+    exit 1
+fi
